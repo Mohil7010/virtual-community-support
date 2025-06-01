ce658ffc5734876c264ba8b39cb93a61760316aa
Mohil Shastri <mohilbhaishastri@gmail.com>
2025-06-01 15:13:07 +0530

    Added Day-5_BooksApi

---
+++ Day-5_BooksApi/BooksApi/BooksApi.Services/Services/Interface/IUserService.cs
@@ -0,0 +1,16 @@
+﻿
+using BooksApi.Entities.Entities;
+using BooksApi.Models;
+
+using System.Threading.Tasks;
+using UserEntity = BooksApi.Entities.Entities.User;
+
+namespace BooksApi.Services.Services.Interface
+{
+    public interface IUserService
+    {
+        Task AddUser(UserEntity user);
+        UserEntity? Login(string username, string password);
+    }
+}
+
---
+++ Day-5_BooksApi/BooksApi/BooksApi.Services/Services/UserService.cs
@@ -0,0 +1,29 @@
+﻿using UserEntity = BooksApi.Entities.Entities.User;
+using BooksApi.Entities.Repositories.Interface;
+using BooksApi.Models;
+using BooksApi.Services.Services.Interface;
+
+namespace BooksApi.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        // Add a user
+        public async Task AddUser(UserEntity user)
+        {
+            await _userRepository.AddUser(user);
+        }
+
+        // User login
+        public UserEntity? Login(string username, string password)
+        {
+            return _userRepository.Login(username, password);
+        }
+    }
+}
